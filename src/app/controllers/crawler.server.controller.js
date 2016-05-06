c01ba1f6732f797503feeaf605eda2a2f6738708
Gokul T P <gokul@klenty.com>
2016-05-06 20:10:06 +0530

    enables cron

---
+++ src/app/controllers/crawler.server.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import {CronJob} from 'cron';
+
+var consistencyCron = new CronJob({
+	cronTime: '/10 * * * * *',
+	onTick:()=>{
+		console.log("Done---------------");
+	},
+	start:true
+});
+
+export function runCron(){
+	consistencyCron.start();
+}
