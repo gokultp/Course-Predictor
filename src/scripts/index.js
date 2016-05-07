a353a6a6fcc6516642cd17fcffbb6286dbed9643
Gokul T P <gokul@klenty.com>
2016-05-07 09:17:36 +0530

    gulp fix for concat react files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,10 @@
+/*eslint-disable no-unused-vars*/
 import React from 'react';
+/*eslint-disable no-unused-vars*/
 import {render} from 'react-dom';
+/*eslint-disable no-unused-vars*/
 import App from './App';
+/*eslint-disable no-unused-vars*/
 
 render(
   <App />,
